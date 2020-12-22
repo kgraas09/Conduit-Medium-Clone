636b88eed8cf9dff2cef86a29cf8437fd9d53131
Kyle Graas <kgraas09@gmail.com>
2020-12-22 11:04:06 -0600

    Added redux with reducer and defined store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import ReactDOM from 'react-dom';
 import React, { useState, useEffect } from 'react';
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
 
 const App = () => {
   return (
